765e7b5e73e10e3498a07f0961d2acff19f1612e
dayat <dayat@gmail.com>
2023-08-06 11:56:01 +0700

    comentar

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
+//   test
 }
 
 export default MyApp;
